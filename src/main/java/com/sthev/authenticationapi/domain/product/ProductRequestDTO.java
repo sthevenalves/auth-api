01dae0ee5d56acc3d2e276f496a62be87499f6ba
Stheven <sthevenalvess@gmail.com>
2024-02-09 21:12:17 -0300

    feat: create Product DTOs

---
+++ src/main/java/com/sthev/authenticationapi/domain/product/ProductRequestDTO.java
@@ -0,0 +1,10 @@
+package com.sthev.authenticationapi.domain.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRequestDTO(
+        @NotBlank
+        String name,
+        @NotNull
+        Integer price) {}
