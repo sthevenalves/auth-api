5f5b50f4d74e4a6bd9407b4e0488eadd17134387
Stheven <sthevenalvess@gmail.com>
2024-02-09 21:16:57 -0300

    feat: create User Repository

---
+++ src/main/java/com/sthev/authenticationapi/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sthev.authenticationapi.repositories;
+
+import com.sthev.authenticationapi.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+
+}
