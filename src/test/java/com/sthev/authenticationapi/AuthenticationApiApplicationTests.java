a8f8072dcc51355b280991606919c1d094fd0844
Stheven <sthevenalvess@gmail.com>
2024-02-09 21:10:45 -0300

    initial config

---
+++ src/test/java/com/sthev/authenticationapi/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthev.authenticationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
