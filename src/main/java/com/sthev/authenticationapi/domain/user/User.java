32ba9819a980056fef435072f10a8d4ed8ba1102
Stheven <sthevenalvess@gmail.com>
2024-02-12 10:55:02 -0300

    feat: create entity User

--- src/main/java/com/sthev/authenticationapi/domain/user/User.java
+++ src/main/java/com/sthev/authenticationapi/domain/user/User.java
@@ -1,7 +1,6 @@
 package com.sthev.authenticationapi.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,10 +17,18 @@ import java.util.List;
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
     private UserRole role;
+
+    public User(String login, String encryptPassword, UserRole role) {
+        this.login = login;
+        this.password = encryptPassword;
+        this.role = role;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
